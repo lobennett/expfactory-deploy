f88d766e6132edd7ce86159800ce2bf568243260
Logan Bennett <bennett.j.logan@gmail.com>
2025-02-25 15:26:51 -0800

    feat: add parser args, update web config for args, add outpaths to POST request

--- expfactory_deploy_local/src/expfactory_deploy_local/serve.py
+++ expfactory_deploy_local/src/expfactory_deploy_local/serve.py
@@ -5,9 +5,10 @@ import sys
 from pathlib import Path
 
 from .utils import generate_experiment_context
-
 import web
 from web.contrib.template import render_jinja
+
+from .preprocess import create_events_file, rename_task
 
 web.config.debug = False
 
@@ -22,26 +23,56 @@ session = web.session.Session(
     initializer={"incomplete": None},
 )
 
-parser = argparse.ArgumentParser(description="Start a local deployment of a battery")
-group = parser.add_mutually_exclusive_group()
-group.add_argument(
-    "exp_config",
-    metavar="EXP_config",
-    type=Path,
-    help="Path to a single experiment or path to a configuration file. Configuration file should be a single path to an experiment per line.",
-    nargs="?",
-)
-group.add_argument(
-    "-e",
-    "--exps",
-    help="Comma delimited list of paths to experiments. Mutually exclusive with exp_config",
-    type=lambda x: [Path(y) for y in x.split(",")],
-)
-parser.add_argument(
-    "-gi",
-    "--group_index",
-    help="Inject a group_index variable into the experiment context.",
-)
+
+def create_parser() -> argparse.ArgumentParser:
+    """Create an argument parser for the serve command.
+
+    Returns:
+        argparse.ArgumentParser: The argument parser.
+    """
+    parser = argparse.ArgumentParser(
+        description="Start a local deployment of a battery"
+    )
+
+    # Move exp_config into the mutually exclusive group AFTER creating it
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "-e",
+        "--exps",
+        help="Comma delimited list of paths to experiments. Mutually exclusive with exp_config",
+        type=lambda x: [Path(y) for y in x.split(",")],
+    )
+    group.add_argument(
+        "-c",
+        "--config",
+        metavar="EXP_CONFIG",
+        type=Path,
+        help=(
+            "Path to a single experiment or path to a configuration file. "
+            "Configuration file should be a single path to an experiment per line."
+        ),
+    )
+    parser.add_argument(
+        "-gi",
+        "--group_index",
+        help="Inject a group_index variable into the experiment context.",
+    )
+
+    # NOTE: Below are additional arguments
+    # for formatting the data outputs into
+    # directories and files that follow BIDS naming conventions.
+    parser.add_argument("-sub", "--subject_id", help="Subject ID")
+    parser.add_argument("-ses", "--session_num", help="Session Number")
+    parser.add_argument("-run", "--run_num", help="Run Number")
+
+    # Output paths for BIDS and RAW data
+    parser.add_argument("-raw", "--raw_dir", help="Path to raw data")
+    parser.add_argument("-bids", "--bids_dir", help="Path to bids data")
+
+    return parser
+
+
+parser = create_parser()
 
 experiments = []
 
@@ -85,7 +116,19 @@ def run(args=None):
             os.unlink(Path(experiments_dir, experiment.stem))
             os.symlink(experiment, Path(experiments_dir, experiment.stem))
 
-    web.config.update({"experiments": experiments})
+    config_updates = {
+        "experiments": experiments,
+    }
+
+    # Add params if not None
+    optional_params = ["raw_dir", "bids_dir", "subject_id", "session_num", "run_num"]
+    for param in optional_params:
+        value = getattr(args, param)
+        if value is not None:
+            config_updates[param] = value
+
+    web.config.update(config_updates)
+
     if args.group_index is not None:
         web.config.update({"group_index": args.group_index})
 
@@ -138,12 +181,69 @@ class serve:
 
     def POST(self):
         exp_name = session.incomplete.pop()
-        date = datetime.datetime.utcnow().strftime("%y-%m-%d-%H:%M")
-        output_file = f"{exp_name}_{date}.json"
-        with open(output_file, "ab") as fp:
+        date = str(int(datetime.datetime.now(datetime.timezone.utc).timestamp()))
+        exp_name_stem = rename_task(exp_name.stem)
+
+        raw_datafile = f"task-{exp_name_stem}_dateTime-{date}.json"
+        bids_datafile = f"task-{exp_name_stem}.tsv"
+
+        # Add bids naming conventions if provided
+        if web.config.session_num:
+            raw_datafile = f"ses-{web.config.session_num}_{raw_datafile}"
+            bids_datafile = f"ses-{web.config.session_num}_{bids_datafile}"
+        if web.config.subject_id:
+            raw_datafile = f"sub-{web.config.subject_id}_{raw_datafile}"
+            bids_datafile = f"sub-{web.config.subject_id}_{bids_datafile}"
+        if web.config.run_num:
+            raw_datafile = raw_datafile.replace(
+                ".json", f"_run-{web.config.run_num}.json"
+            )
+            bids_datafile = bids_datafile.replace(
+                ".tsv", f"_run-{web.config.run_num}.tsv"
+            )
+
+        if web.config.raw_dir:
+            if web.config.subject_id:
+                sub_dir = os.path.join(
+                    web.config.raw_dir, f"sub-{web.config.subject_id}"
+                )
+                if web.config.session_num:
+                    sub_ses_dir = os.path.join(
+                        sub_dir, f"ses-{web.config.session_num}", "func"
+                    )
+                    os.makedirs(sub_ses_dir, exist_ok=True)
+                    raw_datafile = os.path.join(sub_ses_dir, raw_datafile)
+                else:
+                    raw_datafile = os.path.join(sub_dir, raw_datafile)
+            else:
+                raw_datafile = os.path.join(web.config.raw_dir, raw_datafile)
+
+        if web.config.bids_dir:
+            if web.config.subject_id:
+                sub_dir = os.path.join(
+                    web.config.bids_dir, f"sub-{web.config.subject_id}"
+                )
+                if web.config.session_num:
+                    sub_ses_dir = os.path.join(
+                        sub_dir, f"ses-{web.config.session_num}", "func"
+                    )
+                    os.makedirs(sub_ses_dir, exist_ok=True)
+                    bids_datafile = os.path.join(sub_ses_dir, bids_datafile)
+                else:
+                    bids_datafile = os.path.join(sub_dir, bids_datafile)
+            else:
+                bids_datafile = os.path.join(web.config.bids_dir, bids_datafile)
+
+        with open(raw_datafile, "ab") as fp:
             data = web.data()
             fp.write(data)
+            print("Saved raw datafile to: ", raw_datafile)
+
         web.header("Content-Type", "application/json")
+
+        if "practice" not in exp_name_stem:
+            create_events_file(web.data(), bids_datafile)
+
         return "{'success': true}"
 
 
